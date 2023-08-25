705ced68e5713261767de3d91301543fab8e91d5
Mboumela Elton <mboumelae@gmail.com>
2023-08-25 19:19:23 +0100

    last push

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ app.get('/api/get-comment', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`API listening on PORT ${PORT} `)
 });
